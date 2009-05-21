b381adee1e2483479c7f4d42eeb9f7332e34d0ad
giayrus73 <giayrus73@834eae66-9e05-11dd-8bdc-dd70ed116f32>
2009-05-21 17:44:11 +0000

    Documentada case EventQueue

--- desastres/src/java/gsi/simulator/EventQueue.java
+++ desastres/src/java/gsi/simulator/EventQueue.java
@@ -1,25 +1,35 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package gsi.simulator;
 
 import java.util.List;
 import java.util.ArrayList;
 
 /**
+ * Event queue.
+ * Events can be inserted in any order, but they must be extracted by order
+ * (just like method Event.beforeThan).
+ * If two events occur at the same time, the first which was inserted will be extracted.
  *
  * @author Luis Delgado
  */
 public class EventQueue {
-    
-private List<Event> queue;
 
+    /**
+     * The queue where events will be inserted
+     */
+    private List<Event> queue;
+
+    /**
+     * Creates a new unlimited size event queue
+     */
     public EventQueue() {
         queue = new ArrayList<Event>();
     }
 
+    /**
+     * Inserts one event into the queue. If event is null, nothing happens
+     *
+     * @param event event to insert
+     */
     public void insert(Event event) {
         if (event == null) {
             return;
@@ -37,22 +47,43 @@ private List<Event> queue;
         queue.add(0, event);
     }
 
+    /**
+     * It extracts the first non yet extracte event (by time order). If two events
+     * occur at the same time, it selects the first which was inserted
+     *
+     * @return first event in the queue; or NULL if it's empty
+     */
     public Event extract() {
         if (isEmpty())
             return null;
         return queue.remove(0);
     }
 
+    /**
+     * This tells us if the queue is empty
+     *
+     * @return TRUE if the queue is empty
+     */
     public boolean isEmpty() {
         return (queue.size() == 0);
     }
 
+    /**
+     * Gets the number of events inside the queue
+     *
+     * @return number of events inside the queue
+     */
     public int getEventNumber() {
         return (queue.size());
     }
 
 
     @Override
+    /**
+     * List of events inside the queue
+     *
+     * @return all events inside the queue
+     */
     public String toString() {
         if (isEmpty())
             return null;
